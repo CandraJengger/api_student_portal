d1cc184fae9c5da4704ff13a988f66db98d764f1
CandraJengger <cakejengger@gmail.com>
2020-11-12 13:47:55 +0700

    Update Routes to room

--- routes/room-routes.js
+++ routes/room-routes.js
@@ -3,5 +3,9 @@ const r = Router()
 const roomService = require('../service/room-service')
 
 r.get('/', roomService.findAll)
+r.post('/', roomService.findById)
+r.post('/add', roomService.insert)
+r.put('/update', roomService.update)
+r.delete('/delete', roomService.destroy)
 
 module.exports = r
