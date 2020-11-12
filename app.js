c8ed381a3ebfc3ea62270d27bcc6150fa69c0a3d
CandraJengger <cakejengger@gmail.com>
2020-11-13 06:02:50 +0700

    Use semesterRoutes and BatchRoutes

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const buildingRoutes = require('./routes/building-routes')
 const roomRoutes = require('./routes/room-routes')
 const majoringRoutes = require('./routes/majoring-routes')
 const studyProgramRoutes = require('./routes/study-program-routes')
+const semesterRoutes = require('./routes/semester-routes')
+const batchRoutes = require('./routes/batch-routes')
 
 const app = express()
 
@@ -18,6 +20,8 @@ app.use('/buildings', buildingRoutes)
 app.use('/rooms', roomRoutes)
 app.use('/majorings', majoringRoutes)
 app.use('/studyPrograms', studyProgramRoutes)
+app.use('/semesters', semesterRoutes)
+app.use('/batchs', batchRoutes)
 
 const start = () => {
   app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
