4f98f682823e6c0686e1e41fd767899134043b18
CandraJengger <cakejengger@gmail.com>
2020-11-13 06:01:06 +0700

    Add new routes: batch-routes

---
+++ routes/batch-routes.js
@@ -0,0 +1,11 @@
+const Router = require('express').Router
+const r = Router()
+const batchService = require('../service/batch-service')
+
+r.get('/', batchService.findAll)
+r.post('/', batchService.findById)
+r.post('/add', batchService.insert)
+r.put('/update', batchService.update)
+r.delete('/delete', batchService.destroy)
+
+module.exports = r
