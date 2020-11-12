fbd60f65913efefe7a262f92a5598d4d2d6bdc4e
CandraJengger <cakejengger@gmail.com>
2020-11-12 20:08:11 +0700

    Add new routes: majoring-routes and study-program-routes

---
+++ routes/majoring-routes.js
@@ -0,0 +1,11 @@
+const Router = require('express').Router
+const r = Router()
+const majoringService = require('../service/majoring-service')
+
+r.get('/', majoringService.findAll)
+r.post('/', majoringService.findById)
+r.post('/add', majoringService.insert)
+r.put('/update', majoringService.update)
+r.delete('/delete', majoringService.destroy)
+
+module.exports = r
---
+++ routes/study-program-routes.js
@@ -0,0 +1,11 @@
+const Router = require('express').Router
+const r = Router()
+const studyProgramService = require('../service/study-program-service')
+
+r.get('/', studyProgramService.findAll)
+r.post('/', studyProgramService.findById)
+r.post('/add', studyProgramService.insert)
+r.put('/update', studyProgramService.update)
+r.delete('/delete', studyProgramService.destroy)
+
+module.exports = r
