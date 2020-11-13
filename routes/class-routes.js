f04c39c24338db1c1c71dedb5b08cb7943278522
CandraJengger <cakejengger@gmail.com>
2020-11-13 19:08:10 +0700

    Add new routes: class-routes and lectures-routes

---
+++ routes/class-routes.js
@@ -0,0 +1,11 @@
+const Router = require('express').Router
+const r = Router()
+const classService = require('../service/class-service')
+
+r.get('/', classService.findAll)
+r.post('/', classService.findById)
+r.post('/add', classService.insert)
+r.put('/update', classService.update)
+r.delete('/delete', classService.destroy)
+
+module.exports = r
---
+++ routes/lectures-routes.js
@@ -0,0 +1,11 @@
+const Router = require('express').Router
+const r = Router()
+const lecturesService = require('../service/lectures-service')
+
+r.get('/', lecturesService.findAll)
+r.post('/', lecturesService.findById)
+r.post('/add', lecturesService.insert)
+r.put('/update', lecturesService.update)
+r.delete('/delete', lecturesService.destroy)
+
+module.exports = r
