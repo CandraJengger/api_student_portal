2343883fffb2511f5db692fbd775a5408d653336
CandraJengger <cakejengger@gmail.com>
2020-11-14 03:32:22 +0700

    Update models: StudentStatusModel relationMappings

--- models/StudentStatusModel.js
+++ models/StudentStatusModel.js
@@ -2,6 +2,7 @@ const { Model } = require('objection')
 const knex = require('../db/connection')
 const BatchModel = require('./BatchModel')
 const StudentAccountModel = require('./StudentAccountModel')
+const AdminAccountModel = require('./AdminAccountModel')
 
 Model.knex(knex)
 
@@ -26,6 +27,14 @@ class StudentStatusModel extends Model {
         join: {
           from: 't_status_mhs.npm',
           to: 't_akun_mhs.npm'
+        }
+      },
+      admins: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: AdminAccountModel,
+        join: {
+          from: 't_status_mhs.id_admin',
+          to: 't_admin.id_admin'
         }
       }
     }
