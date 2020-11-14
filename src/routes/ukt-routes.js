de2622650e522335125c133b7c6617407fba5b29
CandraJengger <cakejengger@gmail.com>
2020-11-15 06:34:37 +0700

    Add new routes: ukt-routes

---
+++ src/routes/ukt-routes.js
@@ -0,0 +1,11 @@
+const Router = require('express').Router
+const r = Router()
+const uktService = require('../service/ukt-service')
+
+r.get('/', uktService.findAll)
+r.post('/', uktService.findById)
+r.post('/add', uktService.insert)
+r.put('/update', uktService.update)
+r.delete('/delete', uktService.destroy)
+
+module.exports = r
